871d62d22df15f30583fc4cf20cf6b39f1f30ee1
Logan Ryan Golema <partnerships@lrgeometry.us>
2018-08-20 11:00:13 -0500

    Update SafeMath.sol
    post TLS Audit

--- base/SafeMath.sol
+++ base/SafeMath.sol
@@ -46,7 +46,59 @@ library SafeMath {
 
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
     uint256 c = a + b;
+    assert(c >= a);pragma solidity ^0.4.18;
+
+/*
+
+  Copyright 2017 HERC SEZC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
+*/
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+    uint256 c = a * b;
+    assert(c / a == b);
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
     assert(c >= a);
+    return c;
+  }
+}
+
     return c;
   }
 }
