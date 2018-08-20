507028c6d39b9d7cc307ae4082c49887d9856b55
Logan Ryan Golema <partnerships@lrgeometry.us>
2018-08-20 11:05:12 -0500

    (H)ERC777
    post TLS Audit

---
+++ herc-erc777-token/contracts/HERCTokenERC777.sol
@@ -0,0 +1,97 @@
+/*
+Copyright (c) 2018 HERC SEZC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+pragma solidity 0.4.24;
+
+import { ERC777ERC20BaseToken } from "./ERC777/ERC777ERC20BaseToken.sol";
+import { Ownable } from "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+// todo: remove
+// old: contract HERCToken is Token("HERC", "Hercules", 18, 234259085), ERC20, ERC223 {
+//
+
+// HERC Token ERC777
+
+contract HERCToken is ERC777ERC20BaseToken, Ownable {
+    address private mBurnOperator;
+
+    constructor(
+        address[] _defaultOperators,
+        address _burnOperator
+    )
+    ERC777ERC20BaseToken(
+        "Hercules", // name
+        "HERC", // symbol
+        1, // granularity
+        _defaultOperators
+    ) public {
+        bytes memory operatorData = new bytes(0);
+        mint(msg.sender, 234259085, operatorData);
+        mBurnOperator = _burnOperator;
+    }
+
+    /// @notice Disables the ERC20 interface. This function can only be called
+    ///  by the owner.
+    function disableERC20() public onlyOwner {
+        mErc20compatible = false;
+        setInterfaceImplementation("ERC20Token", 0x0);
+    }
+
+    /// @notice Re enables the ERC20 interface. This function can only be called
+    ///  by the owner.
+    function enableERC20() public onlyOwner {
+        mErc20compatible = true;
+        setInterfaceImplementation("ERC20Token", this);
+    }
+
+    /* -- Mint And Burn Functions (not part of the ERC777 standard, only the Events/tokensReceived call are) -- */
+    //
+    /// @notice Generates `_amount` tokens to be assigned to `_tokenHolder`
+    ///  Sample mint function to showcase the use of the `Minted` event and the logic to notify the recipient.
+    /// @param _tokenHolder The address that will be assigned the new tokens
+    /// @param _amount The quantity of tokens generated
+    /// @param _operatorData Data that will be passed to the recipient as a first transfer
+    function mint(address _tokenHolder, uint256 _amount, bytes _operatorData) public onlyOwner {
+        requireMultiple(_amount);
+        mTotalSupply = mTotalSupply.add(_amount);
+        mBalances[_tokenHolder] = mBalances[_tokenHolder].add(_amount);
+
+        callRecipient(msg.sender, 0x0, _tokenHolder, _amount, "", _operatorData, true);
+
+        emit Minted(msg.sender, _tokenHolder, _amount, _operatorData);
+        if (mErc20compatible) { emit Transfer(0x0, _tokenHolder, _amount); }
+    }
+
+    /// @notice Burns `_amount` tokens from `_tokenHolder`
+    ///  Silly example of overriding the `burn` function to only let the owner burn its tokens.
+    ///  Do not forget to override the `burn` function in your token contract if you want to prevent users from
+    ///  burning their tokens.
+    /// @param _amount The quantity of tokens to burn
+    function burn(uint256 _amount, bytes _holderData) public onlyOwner {
+        super.burn(_amount, _holderData);
+    }
+
+    /// @notice Burns `_amount` tokens from `_tokenHolder` by `_operator`
+    ///  Silly example of overriding the `operatorBurn` function to only let a specific operator burn tokens.
+    ///  Do not forget to override the `operatorBurn` function in your token contract if you want to prevent users from
+    ///  burning their tokens.
+    /// @param _tokenHolder The address that will lose the tokens
+    /// @param _amount The quantity of tokens to burn
+    function operatorBurn(address _tokenHolder, uint256 _amount, bytes _holderData, bytes _operatorData) public {
+        require(msg.sender == mBurnOperator);
+        super.operatorBurn(_tokenHolder, _amount, _holderData, _operatorData);
+    }
+}
