f41cff21ace4c9c67b546f4a614a31c4df51886f
VV <vv@neo>
2018-12-23 15:19:05 +0200

    PlayerScore payout

--- assetVerification/contracts/PlayerScore.sol
+++ assetVerification/contracts/PlayerScore.sol
@@ -2,12 +2,14 @@ pragma solidity 0.4.24;
 
 import "./base/Ownable.sol";
 
+import "./herc/HERCToken.sol";
+
 // A contract to manage players' top scores.
 contract PlayerScore is Ownable
 {
     // Represents the maximum amount of
     // stored top scores.
-    uint constant m_maxScores = 5;
+    uint constant m_maxScores = 50;
     
     // Represents the player-score entry.
     struct Score
@@ -83,4 +85,141 @@ contract PlayerScore is Ownable
     {
         return TopScores.length;
     }
+    // SECURE [
+
+    function SetScoreSecure() 
+        public
+        onlyOwner
+    {
+        // todo: review requirements
+    }
+
+    // SECURE ]
+
+    // PAYOUT LOGIC [
+
+    // CONFIGURE PAYOUTS [
+
+    address _hercContract;
+    address _payoutBoss;
+    uint[m_maxScores] _winnerReward;
+
+    function SetHERCTokenAddress(address hercContract)
+        public
+        onlyOwner
+    {
+        _hercContract = hercContract;
+    }
+
+    function SetPayoutAddress(address boss)
+        public
+        onlyOwner
+    {
+        _payoutBoss = boss;
+    }
+
+    function SetWinnerReward(rank, reward)
+        public
+        onlyOwner
+    {
+        _winnerReward[rank] = reward;
+    }
+
+    // CONFIGURE PAYOUTS ]
+    // SEASON PAYOUT [
+
+    function PayoutToWinners()
+        public
+        onlyOwner
+    {
+        require (IsSeasonOver(), "Season in progress");
+
+        // update season
+        SetNextSeasonReleaseDate(_releaseDate + _seasonInterval);
+
+        // move scores to memory for sort and progress
+        Score[] memory scores;
+        uint count = GetTopScoresCount();
+        for (uint i = 0; i < count; i++) {
+            Score score = TopScores[i];
+            scores.push(score);
+        }
+        
+        sortScores(scores);
+
+        // clean up scores for prevet double spent
+        WipeScores();
+
+        for (uint i = 0; i < count; i++) {
+            payoutScore(scores[i].player, i);
+        }
+    }
+
+    event WinnerPayout(player, rank, reward);
+
+    function payoutScore(address player, uint rank)
+        internal
+        onlyOnwer
+    {
+        uint reward = _winnerReward[rank];
+
+        HERCToken(_hercContract).approve(_payoutBoss, reward);
+
+        ERC20(tracker_0x_address).transferFrom(_payoutBoss, player, reward);
+
+        emit WinnerPayout(player, rank, reward);
+    }
+
+    function WipeScores()
+        public
+        onlyOwner
+    {
+        TopScores.length = 0;
+    }
+
+    // SEASON PAYOUT ]
+    // SEASON DATE [
+
+    uint _releaseDate;
+    uint _seasonInterval;
+
+    function SetNextSeasonReleaseDate(uint date)
+        public
+        onlyOwner
+    {
+        _releaseDate = date;
+    }
+
+    function SetSeasonInterval(uint interval)
+        public
+        onlyOwner
+    {
+        _seasonInterval = interval;
+    }
+
+    function IsSeasonOver()
+        public
+    {
+        return (now >= _releaseDate);
+    }
+
+    // SEASON DATE ]
+    // UTILS [
+
+    function sortScores(Score[] memory scores)
+        internal
+    {
+        for(uint i = 0; i < l; i++) {
+            for(uint j = i+1; j < l ;j++) {
+                if(scores[i] > scores[j]) {
+                    Score temp = scores[i];
+                    scores[i] = scores[j];
+                    scores[j] = temp;
+                }
+            }
+        }
+    }
+
+    // UTILS ]
+    // PAYOUT LOGIC ]
 }
